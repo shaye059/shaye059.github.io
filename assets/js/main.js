e7d2bc23bf7b11b87bb6494d77dec38aefa47f72
shaye059 <spencer.hayes.laverdiere@gmail.com>
2020-09-02 19:01:25 -0400

    Tab Feature Update 2

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,20 +4,6 @@
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
 
-$(function() {
-	// Get page title
-	var pageTitle = $("title").text();
-
-	// Change page title on blur
-	$(window).blur(function() {
-		$("title").text("Where Did You Go?");
-	});
-
-	// Change page title back on focus
-	$(window).focus(function() {
-		$("title").text(pageTitle);
-	});
-});
 
 (function($) {
 
